1244415a9b0a8eddc19aea0fa528d4a46fe57734
Colter Ulrich <cju5025@gmail.com>
2021-02-07 16:34:11 -0700

    Styles buttons

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,7 @@ export default function Home () {
             <button>Sell Sounds</button>
             <button>Purchase Sounds</button>
             <img id="home-background-image" src="https://i.ibb.co/fNnBxJ2/techy.png" />
+            <img id="alien" src="https://i.ibb.co/FYw5dpd/alien-146108.png" />
         </div>
     )
 }
