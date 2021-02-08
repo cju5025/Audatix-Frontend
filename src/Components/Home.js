abcfe21b75804cc09837340bdaa332959f687dfc
Colter Ulrich <cju5025@gmail.com>
2021-02-07 18:57:35 -0700

    Adds token to local storage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 export default function Home () {
+    console.log(localStorage.getItem('token'))
     return (
         <div id="home">
             <p>Ready to make a living by doing what you love?</p>
