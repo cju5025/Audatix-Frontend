06500cfa88ef4cd0aa8e0d2c41510611096311eb
Colter Ulrich <cju5025@gmail.com>
2021-02-08 18:29:27 -0700

    Gets link form successful upload post

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -3,10 +3,27 @@ import './Upload.css';
 
 export default class Upload extends Component {
 
+    state = {
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+        fetch('http://localhost:4000/upload', {
+            method: 'POST', 
+            body: new FormData(event.target)
+        }).then(response => response.json())
+            .then(({ data, error }) => {
+                console.log(data)
+            })
+    }
+
     render () {
         return (
-            <form enctype="multipart/form-data" id="upload-form">
-                <input type="file" />
+            <form onSubmit={this.handleSubmit} encType="multipart/form-data" id="upload-form">
+                <input type="file" name="file" />
+
+                <input id="upload-submit" type="submit" value="Upload File" />
             </form>
         )
     }
