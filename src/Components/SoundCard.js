6d72260d0e6569198ac13f2b24d44d4a150bf048
Colter Ulrich <cju5025@gmail.com>
2021-02-11 08:41:34 -0700

    Fixes soundCard layout and styling

--- src/Components/SoundCard.js
+++ src/Components/SoundCard.js
@@ -1,12 +1,20 @@
-
+import './SoundCard.css';
 export default function SoundCard (props) {
     return (
         <div id="sound-card">
+        <div id="name">
             <h1>{props.sound.name}</h1>
+        </div>
+        <div id="category">
             <h2>{props.sound.category}</h2>
+        </div>
+        <div id="price">
             <h3>${props.sound.price}</h3>
+        </div>
+        <div id="button-and-audio">
             <button>Add To Cart</button>
             <audio src={props.sound.location} controls />
+        </div>
         </div>
         
     )
