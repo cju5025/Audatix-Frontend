392f62d1b9f9a08a2d3b498c146d40f37c5d0cab
Colter Ulrich <cju5025@gmail.com>
2021-02-15 10:52:18 -0700

    Styles sound page

--- src/Components/SearchFilter.js
+++ src/Components/SearchFilter.js
@@ -6,7 +6,7 @@ export default function SearchFilter (props) {
         <div id="search-filter-container">
             <h1>Showing 1-25 of 100 sounds</h1>
 
-            <form id="search-filter-form">
+            <form id="sort-form">
                 <label>Sort By</label>
                 <select name="" id="">
                     <option>Alphabetical</option>
@@ -15,24 +15,81 @@ export default function SearchFilter (props) {
                 </select>
             </form>
 
-            <form>
+            <form id="category-form">
                 <label>Category:</label>
                 <br/>
-                    <label htmlFor="film-and-game">Film & Game</label>
+                <div>
                     <input type="checkbox" value="Film & Game" id="film-and-game" />
-
-                    <label htmlFor="electronics">Electronics</label>
+                    <label htmlFor="film-and-game">Film & Game</label>
+                </div>
+                <div>
                     <input type="checkbox" value="Electronics" id="electronics"/>
-
-                    <label htmlFor="machine">Machine</label>
+                    <label htmlFor="electronics">Electronics</label>
+                </div> 
+                <div>
                     <input type="checkbox" value="Machine" id="machine"/>
+                    <label htmlFor="machine">Machine</label>
+                </div>
+                <div>
+                    <input type="checkbox" value="Score" id="score"/>
+                    <label htmlFor="score">Score</label>
+                </div>
             </form>
-            <form>
+            <form id="sub-category-form">
                 <label>Sub-Category</label>
+                <br/>
+                <div>
+                    <input type="checkbox" value="Film & Game" id="film-and-game" />
+                    <label htmlFor="film-and-game">Film & Game</label>
+                </div>
+                <div>
+                    <input type="checkbox" value="Electronics" id="electronics"/>
+                    <label htmlFor="electronics">Electronics</label>
+                </div> 
+                <div>
+                    <input type="checkbox" value="Machine" id="machine"/>
+                    <label htmlFor="machine">Machine</label>
+                </div>
+                <div>
+                    <input type="checkbox" value="Score" id="score"/>
+                    <label htmlFor="score">Score</label>
+                </div>
             </form> 
-            {/* <div id="cart-section">
-                <Cart cartItems={props.cartItems} />
-            </div>        */}
+            <form id="price-form">
+                <label id="price-label">Price:</label>
+                <div id="min-max-container">
+                <div>
+                <label>Min</label>
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+                </div>
+                <div>
+                <label>Max</label>
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+                </div>
+                </div>
+            </form>
         </div>
     )
 }
